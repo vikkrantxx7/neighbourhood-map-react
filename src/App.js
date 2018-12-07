818ebdc17df620172c60c64829ccbb1baa96332a
Vikrant Sharma <vikkrant.xx7@gmail.com>
2018-12-07 16:45:17 +0530

    Load Map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.loadMap()
+  }
+
+  loadMap = () => {
+    window.initMap = this.initMap
+    var index = window.document.getElementsByTagName('script')[0]
+    var script = window.document.createElement('script')
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyB9ggWWErnMrBBV2581LSTZVWxhB7BbylQ&callback=initMap"
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
+  }
+
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 17.422937, lng: 78.6400627},
+      zoom: 14
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map"></div>
+      </main>
     );
   }
 }
