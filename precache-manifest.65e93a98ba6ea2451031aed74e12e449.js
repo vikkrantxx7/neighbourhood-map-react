835bc31ed9171b247aea64529a7b50413e1eaea9
Vikrant Sharma <vikkrant.xx7@gmail.com>
2019-02-08 19:55:05 +0530

    Updates

---
+++ precache-manifest.65e93a98ba6ea2451031aed74e12e449.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95a964457ab9ed597d35",
+    "url": "/neighbourhood-map-react/static/js/runtime~main.95a96445.js"
+  },
+  {
+    "revision": "4d5ef32e7300b9c23698",
+    "url": "/neighbourhood-map-react/static/js/main.4d5ef32e.chunk.js"
+  },
+  {
+    "revision": "847ea33e4c7c76e47245",
+    "url": "/neighbourhood-map-react/static/js/1.847ea33e.chunk.js"
+  },
+  {
+    "revision": "4d5ef32e7300b9c23698",
+    "url": "/neighbourhood-map-react/static/css/main.56a11b99.chunk.css"
+  },
+  {
+    "revision": "82057266fd5a6136892a44273e3d5d1a",
+    "url": "/neighbourhood-map-react/index.html"
+  }
+];
